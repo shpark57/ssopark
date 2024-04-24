36fdb6e0a2cfa10b14abae3fb6301c56dace0213
DESKTOP-4QFCBS1\shpark <emo789@naver.com>
2024-04-24 21:24:34 +0900

    no message

--- server/server/router/file.ts
+++ server/server/router/file.ts
@@ -14,53 +14,53 @@ require("dotenv").config({ path: "/server/.env" })
 const dir = path.join(__dirname, '../../../files/')
 let storage = multer.diskStorage({
     destination: async function  (req, file, cb) {
-      let today = new Date();
-      let year = today.getFullYear()
-      let month = ('0' + (today.getMonth()+1)).slice(-2);
-      let day = ('0' + today.getDate()).slice(-2);
-      let ymd = year+'/'+month+'/'+day+'/'
+        let today = new Date();
+        let year = today.getFullYear()
+        let month = ('0' + (today.getMonth()+1)).slice(-2);
+        let day = ('0' + today.getDate()).slice(-2);
+        let ymd = year+'/'+month+'/'+day+'/'
 
-      const type = req.params.type
-      if(!fs.existsSync(dir+type+'/'+ymd)){
-        await mkdirp(dir+type+'/'+ymd).then((res) => console.log(res)).catch(err=>console.log(err))
-      }
-      await cb(null, dir+type+'/'+ymd);
+        const type = req.params.type
+        if(!fs.existsSync(dir+type+'/'+ymd)){
+            await mkdirp(dir+type+'/'+ymd).then((res) => console.log(res)).catch(err=>console.log(err))
+        }
+        await cb(null, dir+type+'/'+ymd);
     },
     filename: function (req, file, cb) {
-      cb(
-        null,
-        file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-      );
+        cb(
+            null,
+            file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+        );
     },
-  });
-  let fileUpload = multer({
+});
+let fileUpload = multer({
     storage:storage
-  });
+});
 
 
-  router.get('/read/:id', async function(req: Request, res: Response) {
-    const file = await axios.get('/Files/'+req.params.id)
+router.get('/read/:id', async function(req: Request, res: Response) {
+    const file = await axios.get('https://db.sh2.site/api/Files/'+req.params.id)
     if(file.data){
-      const url = dir +file.data.type+'/'+ file.data.ymd + file.data.change_name + '.' +file.data.file_type
-      if(fs.existsSync(url)){
-        const readStream = fs.createReadStream(url)
+        const url = dir +file.data.type+'/'+ file.data.ymd + file.data.change_name + '.' +file.data.file_type
+        if(fs.existsSync(url)){
+            const readStream = fs.createReadStream(url)
 
-        readStream.pipe(res)
-        readStream.on('error',(error) =>{
-          res.status(500).send(error)
-        })
-      }else{
-        res.status(500).send({
-          message: "파일없음"
-        })
-      }
+            readStream.pipe(res)
+            readStream.on('error',(error) =>{
+                res.status(500).send(error)
+            })
+        }else{
+            res.status(500).send({
+                message: "파일없음"
+            })
+        }
     }else{
-      res.status(500).send({
-        message: "파일없음"
-      })
+        res.status(500).send({
+            message: "파일없음"
+        })
     }
-  });
-  
+});
+
 
 
 router.post("/upload/:type", fileUpload.array('files',10) , async function(req: Request, res: Response) {
@@ -81,7 +81,7 @@ router.post("/upload/:type", fileUpload.array('files',10) , async function(req: 
 
         for(let i  in req.files){
             let params = {
-                 parent_id      : req.body['files_params['+i+'].parent_id'] 
+                parent_id      : req.body['files_params['+i+'].parent_id']
                 ,type           : req.body['files_params['+i+'].type']
                 ,type_detail    : req.body['files_params['+i+'].type_detail']
                 ,ymd            : req.body['files_params['+i+'].ymd']
@@ -94,9 +94,9 @@ router.post("/upload/:type", fileUpload.array('files',10) , async function(req: 
             let origin = req.headers.origin
             origin = origin.charAt(origin.length - 1) == '/' ? origin : origin+'/'
 
-            axios.post( '/Files',params)
-        }     
-        
+            axios.post( 'https://db.sh2.site/api/Files',params)
+        }
+
         res.status(201).send({
             message: "파일저장 성공"
         })
@@ -106,7 +106,7 @@ router.post("/upload/:type", fileUpload.array('files',10) , async function(req: 
         })
     }
 
-    
+
 });
 
 
@@ -128,7 +128,7 @@ router.post("/tuiHook/:type", fileUpload.single('file') , async function(req: Re
         let origin = req.headers.origin
         origin = origin.charAt(origin.length - 1) == '/' ? origin : origin+'/'
 
-        axios.post( '/Files',params)
+        axios.post( 'https://db.sh2.site/api/Files',params)
             .then(entity =>{
                 res.status(201).send({
                     message: 'https://db.sh2.site/api/fileService/read/' + entity.data.id
@@ -147,64 +147,64 @@ router.post("/tuiHook/:type", fileUpload.single('file') , async function(req: Re
 router.post('/reallyChange', async function(req: Request, res: Response) {
     try {
 
-    let regex = /\/fileService\/read\/(\d+)/g;
-    let text = req.body['text'];
-    let ids = [];
-    let match;
-    let origin = req.headers.origin
-    origin = origin.charAt(origin.length - 1) == '/' ? origin : origin+'/'
+        let regex = /\/fileService\/read\/(\d+)/g;
+        let text = req.body['text'];
+        let ids = [];
+        let match;
+        let origin = req.headers.origin
+        origin = origin.charAt(origin.length - 1) == '/' ? origin : origin+'/'
 
 
-    while ((match = regex.exec(text)) !== null) {
-        ids.push(match[1]);
-    }
+        while ((match = regex.exec(text)) !== null) {
+            ids.push(match[1]);
+        }
 
 
-    for(let i in ids){
-        axios.get("https://db.sh2.site/api/Files/"+  ids[i] ).then(res =>{
-            const sourceFolder = dir +res.data.type+'/'+ res.data.ymd + res.data.change_name + '.' +res.data.file_type
-            const destinationFolder = dir +req.body['type']+'/'+ res.data.ymd + res.data.change_name + '.' +res.data.file_type
+        for(let i in ids){
+            axios.get("https://db.sh2.site/api/Files/"+  ids[i] ).then(res =>{
+                const sourceFolder = dir +res.data.type+'/'+ res.data.ymd + res.data.change_name + '.' +res.data.file_type
+                const destinationFolder = dir +req.body['type']+'/'+ res.data.ymd + res.data.change_name + '.' +res.data.file_type
 
-            // 폴더 내 파일 목록 가져오기
-            fs.readdir(sourceFolder, (err, files) => {
-                if (err) {
-                    console.error('Error reading directory:', err);
-                    return;
-                }
+                // 폴더 내 파일 목록 가져오기
+                fs.readdir(sourceFolder, (err, files) => {
+                    if (err) {
+                        console.error('Error reading directory:', err);
+                        return;
+                    }
 
-                // 각 파일에 대해 복사 및 삭제 작업 수행
-                files.forEach(file => {
-                    const sourceFile = path.join(sourceFolder, file);
-                    const destinationFile = path.join(destinationFolder, file);
+                    // 각 파일에 대해 복사 및 삭제 작업 수행
+                    files.forEach(file => {
+                        const sourceFile = path.join(sourceFolder, file);
+                        const destinationFile = path.join(destinationFolder, file);
 
-                    // 파일 복사
-                    fs.copyFile(sourceFile, destinationFile, err => {
-                        if (err) {
-                            console.error('Error copying file:', err);
-                        } else {
-                            console.log('File copied successfully:', file);
+                        // 파일 복사
+                        fs.copyFile(sourceFile, destinationFile, err => {
+                            if (err) {
+                                console.error('Error copying file:', err);
+                            } else {
+                                console.log('File copied successfully:', file);
 
-                            // 복사가 완료된 경우, 원본 파일 삭제
-                            fs.unlink(sourceFile, err => {
-                                if (err) {
-                                    console.error('Error deleting file:', err);
-                                } else {
-                                    console.log('File deleted successfully:', file);
-                                }
-                            });
-                        }
+                                // 복사가 완료된 경우, 원본 파일 삭제
+                                fs.unlink(sourceFile, err => {
+                                    if (err) {
+                                        console.error('Error deleting file:', err);
+                                    } else {
+                                        console.log('File deleted successfully:', file);
+                                    }
+                                });
+                            }
+                        });
                     });
-                });
+                })
+
+            }).then(res=>{
+                axios.patch("https://db.sh2.site/api/Files/"+  ids[i]  ,  { type :req.body['type'] } )
             })
 
-        }).then(res=>{
-            axios.patch("/Files/"+  ids[i]  ,  { type :req.body['type'] } )
-        })
-
-        res.status(201).send({
-            message: "파일복제 성공"
-        })
-    }} catch (error) {
+            res.status(201).send({
+                message: "파일복제 성공"
+            })
+        }} catch (error) {
         res.status(500).send({
             message: "복제 실패"
         })
