2ebe64316060c32bc55a5b3713b14bf84b4cc8f1
shpark <shpark91@kakao.com>
2024-04-26 14:31:54 +0900

    no message

--- client/src/pages/payment/Payment.tsx
+++ client/src/pages/payment/Payment.tsx
@@ -15,11 +15,17 @@ const Payment = () => {
     const {query} = useParams();
 
     useEffect(() => {
+<<<<<<< HEAD
         /*
+=======
+
+        // @ts-ignore
+>>>>>>> c52a42c (no message)
         const { imp_uid , merchant_uid , imp_success } = query;
         if(imp_uid && merchant_uid && imp_success) {
             // 결제 후 리디렉션 url로 이동이 되었을 경우
             // ... 쿼리스트링으로 받은 데이터를 가지고 핸들링
+<<<<<<< HEAD
         };
          */
 
@@ -28,8 +34,52 @@ const Payment = () => {
 
     return(
         <div></div>
+=======
+
+            axios.patch(process.env.REACT_APP_SERVER_HOST_API + '/Orders/id='+merchant_uid ,{ 'order_state' : '결제성공'})
+                .then(res=>{
+                    showModal({
+                        modalType: "AlertModal",
+                        modalProps: {
+                            message: "주문에 성공했습니다.",
+                            handleConfirm : arg => {
+                                navigate(String("/carts"))
+                            }
+                        }
+                    });
+                })
+        }else{
+            axios.delete(process.env.REACT_APP_SERVER_HOST_API + '/OrderDetails?order_id='+merchant_uid)
+                .then(res=>{
+                    axios.delete(process.env.REACT_APP_SERVER_HOST_API + '/Order?id='+merchant_uid)
+                        .then(res=>{
+                            showModal({
+                                modalType: "AlertModal",
+                                modalProps: {
+                                    message: "주문에 실패했습니다.",
+                                    handleConfirm : arg => {
+                                        navigate(String("/carts"))
+                                    }
+                                }
+                            });
+                        }).catch(e=>{console.log(e)})
+                }).catch(e=>{console.log(e)})
+        }
+    }, [query]);
+
+    return(
+        <Container component="main" maxWidth="lg">
+            테스트
+        </Container>
+>>>>>>> c52a42c (no message)
     )
 
 }
 
+<<<<<<< HEAD
 export default Payment
+=======
+export default Payment
+
+
+>>>>>>> c52a42c (no message)
