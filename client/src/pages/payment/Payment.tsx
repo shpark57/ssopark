2ad3cd75abf5e98db345d13b0f85ab3aa3e253aa
shpark <shpark91@kakao.com>
2024-04-26 16:50:26 +0900

    결제성공될거같음
    (cherry picked from commit beb67fdcc881a2d3ae7615d1737245b103c3e4c0)

--- client/src/pages/payment/Payment.tsx
+++ client/src/pages/payment/Payment.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState ,useContext} from 'react';
-import { useParams ,useNavigate} from 'react-router-dom';
+import { useSearchParams ,useNavigate} from 'react-router-dom';
 import Button from '@mui/material/Button';
 import * as Time from 'src/types/time'
 import {Container , Grid, Box , TextField} from '@mui/material';
@@ -12,39 +12,53 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import CommentComponent from 'src/components/comment/CommentComponent';
 
 const Payment = () => {
-    const {query} = useParams();
+   alert("결과가 와야해..")
 
+    const { showModal } = useModal();
+    let navigate = useNavigate();   //페이지 이동을 위해필요.
+    const [query, setQuery]  = useSearchParams();
     useEffect(() => {
 
-        // @ts-ignore
-        const { imp_uid , merchant_uid , imp_success } = query;
-        if(imp_uid && merchant_uid && imp_success) {
+        if(query.get('imp_uid') && query.get('merchant_uid') && query.get('imp_success')) {
             // 결제 후 리디렉션 url로 이동이 되었을 경우
             // ... 쿼리스트링으로 받은 데이터를 가지고 핸들링
-
-            axios.patch(process.env.REACT_APP_SERVER_HOST_API + '/Orders/id='+merchant_uid ,{ 'order_state' : '결제성공'})
+            alert("결제변경 갑니다.")
+            axios.patch(process.env.REACT_APP_SERVER_HOST_API + '/Orders?id='+query.get('merchant_uid') ,{ 'order_state' : '결제성공'})
                 .then(res=>{
+
+                    alert("주문도성공")
                     showModal({
                         modalType: "AlertModal",
                         modalProps: {
                             message: "주문에 성공했습니다.",
                             handleConfirm : arg => {
+ /* 장바구니 삭제 로직
+     let cartLocalStorage = window.localStorage;
+    let localCartList =  cartLocalStorage.getItem("localCartList")
+    if(localCartList){
+      let cartList = JSON.parse(localCartList)
+      for(let i in props.carts){
+        cartList = cartList.filter((obj:CartProps , index:number) => obj['product_id'] !== props.carts[i].product_id )
+      }
+      cartLocalStorage.setItem("localCartList",JSON.stringify(cartList))
+    }                               
+  */
                                 navigate(String("/carts"))
                             }
                         }
                     });
                 })
         }else{
-            axios.delete(process.env.REACT_APP_SERVER_HOST_API + '/OrderDetails?order_id='+merchant_uid)
+            axios.delete(process.env.REACT_APP_SERVER_HOST_API + '/OrderDetails?order_id='+query.get('merchant_uid'))
                 .then(res=>{
-                    axios.delete(process.env.REACT_APP_SERVER_HOST_API + '/Order?id='+merchant_uid)
+                    axios.delete(process.env.REACT_APP_SERVER_HOST_API + '/Order?id='+query.get('merchant_uid'))
                         .then(res=>{
                             showModal({
                                 modalType: "AlertModal",
                                 modalProps: {
                                     message: "주문에 실패했습니다.",
                                     handleConfirm : arg => {
-                                        navigate(String("/carts"))
+                                       navigate(String("/carts"))
                                     }
                                 }
                             });
