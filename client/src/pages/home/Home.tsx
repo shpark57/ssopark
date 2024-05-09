29cd4a943ae7886c98867db5c849ef693acfddb7
DESKTOP-4QFCBS1\shpark <emo789@naver.com>
2024-05-09 22:56:38 +0900

    승현네 정보 조금 수정
    (cherry picked from commit 1e07256580414f3232dc57638f0d8312f891aab6)

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -49,19 +49,36 @@ export default function Home(){
 
   return(
         <Container component="main" maxWidth="xs" sx={{textAlign:'center'}}>
+            <div style={{
+                backgroundImage: `url(${process.env.PUBLIC_URL}/images/introduction_3.jpg)`,
+                backgroundSize: '100%',
+                height: '320px',
+                backgroundRepeat: 'no-repeat',
+                marginBottom: '30px',
+                position: 'relative' /* 내부 요소의 위치를 설정하기 위해 부모 요소에 상대적 위치를 지정합니다. */
+            }}>
+                <div style={{
+                    position: 'absolute', /* 내부 요소를 부모 요소에 대해 절대적으로 위치시킵니다. */
+                    top: '50%', /* 위쪽 여백을 50%로 설정하여 요소를 수직으로 가운데로 이동시킵니다. */
+                    left: '50%', /* 왼쪽 여백을 50%로 설정하여 요소를 수평으로 가운데로 이동시킵니다. */
+                    transform: 'translate(-50%, -50%)', /* 수평 및 수직 이동을 위해 요소를 자체 크기의 반만큼 이동시킵니다. */
+                    textAlign: 'center', /* 텍스트를 수평으로 가운데로 정렬합니다. */
+                    backgroundColor: 'rgba(255, 255, 255, 0.7)', /* 배경 색상과 투명도를 지정합니다. */
+                    padding: '10px', /* 내부 여백을 추가합니다. */
+                    width: '90%'
+                }}>
 
-            <p>
-                <strong>
-                    2018년 영농 후 계자로 출발한 저희 농장인 승현네는 귀농 후 고창군의 청정하고 아름다운 자연 속에서
-                </strong>
-            </p>
-            <p>
-                <strong>
-                    수박, 메론, 블루베리, 청량고추 등 다양한 작물을 친환경 재배방식으로 정성껏 재배하고 있습니다.
-                </strong>
-            </p>
+                    <strong>
+                        <p> 2018년 영농 후계자로 출발한  </p>
+                        <p> 농장인 승현네는 귀농 후 고창군의 </p>
+                        <p> 청정하고 아름다운 자연 속에서 </p>
+                        <p> 수박, 메론, 블루베리, 청량고추 등  </p>
+                        <p> 다양한 작물을 친환경 재배방식으로 </p>
+                        <p> 정성껏 재배하고 있습니다. </p>
 
-
+                    </strong>
+                </div>
+            </div>
 
             <Carousel products={products}/>
 
